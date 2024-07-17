0ea3985c93a0a7c2ab06129e090fb4b0bb3e15ba
Ng Khai Xiang <nkxiang5575@gmail.com>
2024-07-17 23:27:40 +0800

    firebase test

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAC95lK841umejYkYUfvGMTmJpXI6-RqBw",
+    authDomain: "todo-app-b073e.firebaseapp.com",
+    projectId: "todo-app-b073e",
+    storageBucket: "todo-app-b073e.appspot.com",
+    messagingSenderId: "47554545879",
+    appId: "1:47554545879:web:a775edfb478140d3a8c892"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,11 +6,12 @@ import {
     signInWithPopup,
 } from "firebase/auth";
 import { useContext, useEffect, useState } from "react";
-import { Col, Button, Modal, Form, Container } from "react-bootstrap";
+import { Col, Button, Modal, Form, Container, Row, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../components/AuthProvider";
 
 export default function LoginPage() {
+    const loginImage = "https://preview.redd.it/drawing-random-tpot-characters-day-3-ice-cube-comment-who-i-v0-5mv39tvoexcb1.png?width=1080&crop=smart&auto=webp&s=08ec985dee611ef7ab1b95433a45869ed8128176"
 
     const [modalShow, setModalShow] = useState(null);
     const handleShowSignUp = () => setModalShow("SignUp");
@@ -60,81 +61,74 @@ export default function LoginPage() {
     };
     const handleClose = () => setModalShow(null);
     return (
-        <Col>
-            <Container>
-                <div className="header">
-                    <div className="text">Log In</div>
-                    <div className="underline"></div>
-                </div>
+        <Row>
+            <Col sm={6}>
+                <Image src={loginImage} fluid />
+            </Col>
+            <Col>
+                <Container>
+                    <div className="header">
+                        <div className="text">Icey Todo App</div>
+                        <div className="underline"></div>
+                    </div>
 
-                <div className="input">
-                    <input type="text" placeholder="Email Id" />
-                </div>
-
-                <div className="input">
-                    <input type="password" placeholder="Password" />
-                </div>
-                <Button className="rounded-pill" style={{ fontSize: 20, fontWeight: "bold" }} onClick={handleShowLogin}>
-                    Log In
-                </Button>
-
-                <Col>
-                    <Button className="rounded-pill" style={{ fontSize: 20, fontWeight: "bold" }} onClick={handleShowSignUp}>
-                        Create an account
+                    <Button className="rounded-pill" style={{ fontSize: 20, fontWeight: "bold" }} onClick={handleShowLogin}>
+                        Sign In
                     </Button>
-                    <Button
-                        className="rounded-pill"
-                        onClick={handleGoogleLogin}
-                    >
-                        <i className="bi bi-google"></i> Sign up with Google
-                    </Button>
-                </Col>
-                <Col>
-                    <Modal
-                        show={modalShow !== null}
-                        onHide={handleClose}
-                        animation={false}
-                        centered
-                    >
-                        <Modal.Body>
-                            <h2 className="mb-4" style={{ fontWeight: "bold" }}>
-                                {modalShow === "SignUp"
-                                    ? "Create your account"
-                                    : "Log in to your account"}
-                            </h2>
-                            <Form
-                                className="d-grid gap-2 px-5"
-                                onSubmit={modalShow === "SignUp" ? handleSignUp : handleLogin}
-                            >
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Control
-                                        onChange={(e) => setUserName(e.target.value)}
-                                        type="email"
-                                        placeholder="Enter Email"
-                                    />
-                                </Form.Group>
 
-                                <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Control
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        type="password"
-                                        placeholder="Enter Password"
-                                    />
-                                </Form.Group>
+                    <Col>
+                        <Button className="rounded-pill" style={{ fontSize: 20, fontWeight: "bold" }} onClick={handleShowSignUp}>
+                            Sign Up
+                        </Button>
+                        <Button
+                            className="rounded-pill"
+                            onClick={handleGoogleLogin}
+                        >
+                            <i className="bi bi-google"></i> Sign up with Google
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Modal
+                            show={modalShow !== null}
+                            onHide={handleClose}
+                            animation={false}
+                            centered
+                        >
+                            <Modal.Body>
+                                <h2 className="mb-4" style={{ fontWeight: "bold" }}>
+                                    {modalShow === "SignUp"
+                                        ? "Create your account"
+                                        : "Log in to your account"}
+                                </h2>
+                                <Form
+                                    className="d-grid gap-2 px-5"
+                                    onSubmit={modalShow === "SignUp" ? handleSignUp : handleLogin}
+                                >
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Control
+                                            onChange={(e) => setUserName(e.target.value)}
+                                            type="email"
+                                            placeholder="Enter Email"
+                                        />
+                                    </Form.Group>
 
-                                <p style={{ fontSize: "15px" }}>
-                                    By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.
-                                    Icey Todo App may use your contact information, including your email address and phone number for purposes outlined in our Privacy Policy.
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Control
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            type="password"
+                                            placeholder="Enter Password"
+                                        />
+                                    </Form.Group>
 
-                                </p>
-                                <Button className="rounded-pill" type="submit">
-                                    {modalShow === "SignUp" ? "Sign up" : "Log in"}
-                                </Button>
-                            </Form>
-                        </Modal.Body>
-                    </Modal>
-                </Col>
-            </Container>
-        </Col>
-    )
+                                    <Button className="rounded-pill" type="submit">
+                                        {modalShow === "SignUp" ? "Sign up" : "Log in"}
+                                    </Button>
+                                </Form>
+                            </Modal.Body>
+                        </Modal>
+                    </Col>
+                </Container>
+            </Col>
+        </Row>
+    );
 }
